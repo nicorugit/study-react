f3a710d16a669252373680fd5661cf7d11d81b26
cioccolata <67297729+nicorugit@users.noreply.github.com>
2023-04-23 00:55:27 +0900

    ✨ アバウトページを追加

---
+++ pages/about.js
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { Footer } from '../components/Footer'
+import Links from '../components/Links'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>about page</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+        About Page  <a href="https://nextjs.org">About.js!</a>
+        </h1>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+
+        <Links  />
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
